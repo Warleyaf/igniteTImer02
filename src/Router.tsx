47d651da2ec33a1d19ecfdee9b186cca5b7ed2c1
warleyaf <warleysilva365@gmail.com>
2022-07-12 20:03:31 -0300

    Adicionando o layout de rotas, que é manter uma página padrão que não se modifica quando alterar de rotas, utilizei também o <Outlet/>

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,12 +3,16 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { History } from './pages/History'
+import { DefaultLayout } from './layouts/DefaultLayout'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
+      {/** Pegando o defaultLayout por element ou seja meio que o meu header vai estar por padrão nos meus components sem que precise renderizar novamente */}
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
     </Routes>
   )
 }
