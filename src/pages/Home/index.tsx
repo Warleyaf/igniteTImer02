5875307acfa4d114f3bea21ad86e0774d0518e15
warleyaf <warleysilva365@gmail.com>
2022-07-30 12:02:12 -0300

    Mostrando que podemos utilizar a validação de formulários, e para isso utilizei as biblioteca zod eo @hookform/resolvers

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,8 @@
 import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form' // aqui importei a função useForm
+// instalando as bibliotecas zod = npm i zod, e para intgrar a bilioteca com o hook form eu instalo o npm i @hookform/resolvers
+import { zodResolver } from '@hookform/resolvers/zod' // fazendo a integração com o zod e o hookform/resolvers
+import * as zod from 'zod' // importando a biblioteca zod, use o * para que ele possa importar tudo do zod, use esse * por que ele não tem o "export default"
 
 import {
   CountdownContainer,
@@ -11,23 +14,28 @@ import {
   TaskInput,
 } from './styles'
 
+const newCycleFormValidationSchema = zod.object({
+  // esse zod.object é por que as minha informções estão vindo no formato de objeto lá do meu handleCreateNewCycle
+  // regras de validação para o meu zodResolver
+  task: zod.string().min(1, 'Informe a tarefa'), // aqui meio que estou falando que só vai validar se na minha task ter ao menos 1 caracter caso não tenha vai me retorbar a string "Informe a tarefa"
+  minutesAmount: zod.number().min(5).max(60),
+})
+
 export function Home() {
-  // esse register nada mais é que um método, que ele vai adicionar um input ao nosso formulário, e quando eu dou esse useForm() é como se eu tivesse criando um novo formulário e afunçlão register fala quais são os campos que eu vou ter no meu formulário.
-
-  const { register, handleSubmit, watch } = useForm()
-  // esse watch meio que eu fico observando alguma coisa
-
+  const { register, handleSubmit, watch } = useForm({
+    // aqui estou passando um objeto de configurações de validação do formulário utilizando o zod
+    // e dentro desse zodResolver eu preciso passar qual é o schema de validação que no caso foi o newCycleFormValidationSchema
+    resolver: zodResolver(newCycleFormValidationSchema),
+  })
   function handleCreateNewCycle(data: any) {
-    // esse data são tipos os dados dos inputs do nosso formulário
     console.log(data)
   }
 
-  const task = watch('task') // aqui meio que estou falando que eu quero observar o campo task que está ai no primeiro form, e dessa forma ai eu consigo saber o falor de task em tempo real
-  const isSubmitDisabled = !task // aqui meio que é uma variável auxiliar que é para facilitar a leitura do código para quem for ver esse código, isso é meio que uma boa prática e para ajudar o próximo na legibilidade do código
+  const task = watch('task')
+  const isSubmitDisabled = !task
 
   return (
     <HomeContainer>
-      {/* aqui no onSubmit meio que estou passando o handleSubmit que está vindo do mpetodo do useForm e passo para dentro do handleSubmit a função do handleCreateNewCicle */}
       <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
@@ -35,7 +43,7 @@ export function Home() {
             id="task"
             list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
-            {...register('task')} // sintaxe do register
+            {...register('task')}
           />
 
           <datalist id="task-suggestions">
@@ -50,9 +58,9 @@ export function Home() {
             type="number"
             id="minutesAmount"
             placeholder="00"
-            step={5} // esse step significa que, toda vez que aumentar meu número vai aumentar de 5 em 5
+            step={5}
             min={5}
-            max={60}
+            // max={60}
             {...register('minutesAmount', { valueAsNumber: true })} // sintaxe do register
           />
 
@@ -66,7 +74,6 @@ export function Home() {
           <span>0</span>
           <span>0</span>
         </CountdownContainer>
-        {/** aqui estou observando a task que esta lá em cima na linha 25, e meio que estou dizendo  se o campo de task for diferente de vazio eu quero habilitar o botão, no caso quando alguém digitar algo no meu task eu habilito o botão */}
         <StartCountdownButton disabled={isSubmitDisabled} type="submit">
           <Play size={24} />
           Começar
